8f1dd2f42a977b84cd400cd815fc8d7b15850b19
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2022-10-16 17:52:00 +0300

    Document current scripting functions

--- src/Dotx64Managed/Scripting/Scripting.cs
+++ src/Dotx64Managed/Scripting/Scripting.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Dotx64Dbg
 {
@@ -20,43 +16,70 @@ namespace Dotx64Dbg
             Console.WriteLine(fmt, args);
         }
 
+        /// <summary>
+        /// Step into.
+        /// </summary>
         public static void Sti()
         {
             Debugger.StepIn();
         }
 
+        /// <summary>
+        /// Step into a specified amount of times.
+        /// </summary>
+        /// <param name="steps">Amount of steps</param>
         public static void Sti(int steps)
         {
             for (int i = 0; i < steps; i++)
                 Sti();
         }
 
+        /// <summary>
+        /// Step over.
+        /// </summary>
         public static void Sto()
         {
             Debugger.StepOver();
         }
 
+        /// <summary>
+        /// Step over a specified amount of times.
+        /// </summary>
+        /// <param name="steps">Amount of steps</param>
         public static void Sto(int steps)
         {
             for (int i = 0; i < steps; i++)
                 Sto();
         }
 
+        /// <summary>
+        /// Continue execution
+        /// </summary>
         public static void Run()
         {
             Debugger.Run();
         }
 
+        /// <summary>
+        /// Pause execution.
+        /// </summary>
         public static void Pause()
         {
             Debugger.Pause();
         }
 
+        /// <summary>
+        /// Stops the debugger
+        /// </summary>
         public static void Stop()
         {
             Debugger.Stop();
         }
 
+        /// <summary>
+        /// Skip over a specified amount of instructions.
+        /// </summary>
+        /// <param name="numInstructions">Amount of instructions to skip</param>
         public static void Skip(int numInstructions = 1)
         {
             Debugger.RunCommand($"skip {numInstructions}");
